131f3af39ee3b70e2875fbd2d4a9bc3ea49aa70b
Vaanya Sehrawat <Vaanyasehrawat2006@gmail.com>
2026-09-15 22:27:27 +0530

    Day 01: Java basics and arithmetic operations

---
+++ Day-01/Q1.java
@@ -0,0 +1,29 @@
+// Q1: Write a program to input two numbers and display their sum.
+
+/*
+Sample Test Cases:
+Input 1:
+3 4
+Output 1:
+Sum = 7
+
+Input 2:
+-1 20
+Output 2:
+Sum = 19
+
+*/
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
+}
